c6e4dd2616a0c6bdfc7a5a6b43609ea13b33c876
Jason Dillon <jason@planet57.com>
2011-07-07 14:49:39 -0700

    re-impl based on contrib from log4j: https://github.com/apache/log4j/blob/trunk/contribs/JimMoore/LoggingOutputStream.java

--- gossip-support/src/main/java/org/sonatype/gossip/support/LoggingOutputStream.java
+++ gossip-support/src/main/java/org/sonatype/gossip/support/LoggingOutputStream.java
@@ -19,83 +19,144 @@ package org.sonatype.gossip.support;
 import org.slf4j.Logger;
 import org.sonatype.gossip.Level;
 
-import java.io.ByteArrayOutputStream;
-import java.io.FilterOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
 /**
  * {@link OutputStream} which logs lines to a {@link Logger}.
  *
- * Optionally will delegate to another {@link OutputStream} after logging if given instance is non-null.
- *
+ * @author <a href="mailto://Jim.Moore@rocketmail.com">Jim Moore</a>
  * @author <a href="mailto:jason@planet57.com">Jason Dillon</a>
  * @since 1.8
+ *
+ * @link https://github.com/apache/log4j/blob/trunk/contribs/JimMoore/LoggingOutputStream.java
  */
 public class LoggingOutputStream
-    extends FilterOutputStream
+    extends OutputStream
 {
-    private final Logger logger;
+    protected static final String LINE_SEPERATOR = System.getProperty("line.separator");
 
-    private final Level level;
+    /**
+     * Used to maintain the contract of {@link #close()}.
+     */
+    protected boolean hasBeenClosed = false;
 
-    private final ByteArrayOutputStream buff = new ByteArrayOutputStream(1024 * 4);
+    /**
+     * The internal buffer where data is stored.
+     */
+    protected byte[] buf;
 
-    private int last;
+    /**
+     * The number of valid bytes in the buffer. This value is always in the range <tt>0</tt> through <tt>buf.length</tt>; elements <tt>buf[0]</tt>
+     * through <tt>buf[count-1]</tt> contain valid byte data.
+     */
+    protected int count;
 
-    public LoggingOutputStream(final Logger logger, final Level level, final OutputStream delegate) {
-        super(delegate);
-        assert logger != null;
-        this.logger = logger;
-        assert level != null;
-        this.level = level;
-    }
+    /**
+     * Remembers the size of the buffer for speed.
+     */
+    private int bufLength;
+
+    /**
+     * The default number of bytes in the buffer. =2048
+     */
+    public static final int DEFAULT_BUFFER_LENGTH = 2048;
+
+    protected Logger logger;
+
+    protected Level level;
 
     public LoggingOutputStream(final Logger logger, final Level level) {
-        this(logger, level, null);
+        if (logger == null) {
+            throw new IllegalArgumentException("cat == null");
+        }
+        if (level == null) {
+            throw new IllegalArgumentException("priority == null");
+        }
+
+        this.level = level;
+        this.logger = logger;
+        bufLength = DEFAULT_BUFFER_LENGTH;
+        buf = new byte[DEFAULT_BUFFER_LENGTH];
+        count = 0;
     }
 
-    public LoggingOutputStream(final Logger logger) {
-        this(logger, Level.INFO);
+
+    /**
+     * Closes this output stream and releases any system resources associated with this stream. The general contract of <code>close</code> is that it
+     * closes the output stream. A closed stream cannot perform output operations and cannot be reopened.
+     */
+    public void close() {
+        flush();
+        hasBeenClosed = true;
     }
 
-    public Logger getLogger() {
-        return logger;
-    }
 
-    public Level getLevel() {
-        return level;
-    }
-
-    @Override
+    /**
+     * Writes the specified byte to this output stream. The general contract for <code>write</code> is that one byte is written to the output stream. The
+     * byte to be written is the eight low-order bits of the argument <code>b</code>. The 24 high-order bits of <code>b</code> are ignored.
+     *
+     * @param b the <code>byte</code> to write
+     * @throws IOException if an I/O error occurs. In particular, an <code>IOException</code> may be thrown if the output stream has been closed.
+     */
     public void write(final int b) throws IOException {
-        if (b == '\r' || b == '\n') {
-            if (!(last != '\r' || b != '\n')) {
-                level.log(logger, buff.toString("UTF-8"));
-                buff.reset();
+        if (hasBeenClosed) {
+            throw new IOException("The stream has been closed.");
+        }
+
+        // don't log nulls
+        if (b == 0) {
+            return;
+        }
+
+        // would this be writing past the buffer?
+        if (count == bufLength) {
+            // grow the buffer
+            final int newBufLength = bufLength + DEFAULT_BUFFER_LENGTH;
+            final byte[] newBuf = new byte[newBufLength];
+
+            System.arraycopy(buf, 0, newBuf, 0, bufLength);
+
+            buf = newBuf;
+            bufLength = newBufLength;
+        }
+
+        buf[count] = (byte) b;
+        count++;
+    }
+
+    /**
+     * Flushes this output stream and forces any buffered output bytes to be written out. The general contract of <code>flush</code> is that calling it
+     * is an indication that, if any bytes previously written have been buffered by the implementation of the output stream, such bytes should
+     * immediately be written to their intended destination.
+     */
+    public void flush() {
+        if (count == 0) {
+            return;
+        }
+
+        // don't print out blank lines; flushing from PrintStream puts out these
+        if (count == LINE_SEPERATOR.length()) {
+            if (((char) buf[0]) == LINE_SEPERATOR.charAt(0) &&
+                ((count == 1) ||  // <- Unix & Mac, -> Windows
+                    ((count == 2) && ((char) buf[1]) == LINE_SEPERATOR.charAt(1)))) {
+                reset();
+                return;
             }
         }
-        else {
-            buff.write(b);
-        }
-        last = b;
 
-        if (out != null) {
-            super.write(b);
-        }
+        final byte[] theBytes = new byte[count];
+
+        System.arraycopy(buf, 0, theBytes, 0, count);
+
+        level.log(logger, new String(theBytes));
+
+        reset();
     }
 
-    @Override
-    public void flush() throws IOException {
-        if (out != null) {
-            super.flush();
-        }
-    }
-
-    @Override
-    public void close() throws IOException {
-        if (out != null) {
-            super.close();
-        }
+    private void reset() {
+        // not resetting the buffer -- assuming that if it grew that it
+        //   will likely grow similarly again
+        count = 0;
     }
 }
