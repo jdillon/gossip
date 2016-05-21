c8a5b1ed56a506d79294ee820f176309be930304
Jason Dillon <jason@planet57.com>
2016-05-21 16:12:51 -0700

    Use getClass().getSimpleName() instead of hard-coded string

--- gossip-bootstrap/src/main/java/com/planet57/gossip/Event.java
+++ gossip-bootstrap/src/main/java/com/planet57/gossip/Event.java
@@ -89,7 +89,7 @@ public final class Event
 
   @Override
   public String toString() {
-    return "Event{" +
+    return getClass().getSimpleName() + "{" +
         "logger=" + logger +
         ", level=" + level +
         ", message='" + message + '\'' +
